f4a4516c54edd55246207302e023a7255a1b18fb
Celocious Altrin <celociousalltrinmf@gmail.com>
2025-03-16 09:25:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { SpreadSheet } from "./components/SpreadSheet";
+
+function App() {
+  return (
+    <>
+      <SpreadSheet />
+    </>
+  );
+}
+
+export default App;
